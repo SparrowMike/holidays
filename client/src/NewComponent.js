f6df01af2ac2d70f0272a5e370cf791c29e30658
SparrowMike <michal.m_wroblewski@hotmail.com>
2021-10-27 15:52:13 +0800

    improvements

--- client/src/NewComponent.js
+++ client/src/NewComponent.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NewComponent = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>this works better now</h1>
+    </div>
+  );
 };
 
 export default NewComponent;
