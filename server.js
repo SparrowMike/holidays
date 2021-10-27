0b67b67ee5e87ef5791b83d5ab219eb380b2e872
SparrowMike <michal.m_wroblewski@hotmail.com>
2021-10-27 14:43:03 +0800

    feat: 🎸 instructions for heorku build

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const path = require("path");
 const port = process.env.PORT ?? 2001;
 
-app.get("/", (req, res) => {
-  res.send("hello there");
-});
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// app.get("/", (req, res) => {
+//   res.send("hello there");
+// });
 
 app.listen(port, (req, res) => {
   console.log("Hi there from port, ", port);
